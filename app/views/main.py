5f219214b6ca53cf578a9cf442c9165ab5997be0
predatorsense03 <janglobio@gmail.com>
2024-10-02 21:43:28 +0800

    modified: extend mysql, added part II sql queries task

--- app/views/main.py
+++ app/views/main.py
@@ -1,7 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-     return render_template('base.html')
+    db = g.db
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM students")
+    students = cursor.fetchall()
+
+    cursor.execute("SELECT fname FROM students WHERE fname RLIKE BINARY '^[A-Z]' LIMIT 1")
+    fname = cursor.fetchone()
+
+    cursor.execute("SELECT DISTINCT(major) FROM students")
+    major = cursor.fetchall()
+
+    cursor.execute("""
+     SELECT fname,
+           CASE
+               WHEN amount = 0 THEN "NULL"
+               ELSE amount
+           END AS scholarship_amount
+    FROM students
+     """)
+    scholar = cursor.fetchall()
+
+
+    cursor.execute("SELECT id, fname FROM students WHERE scholarship = 0")
+    non_scholar = cursor.fetchall()
+    cursor.close()
+
+    return render_template('index.html', students=students, fname=fname, major=major, scholar=scholar, non_scholar=non_scholar)
